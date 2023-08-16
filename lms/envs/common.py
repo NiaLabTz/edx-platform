578c8d05673d2137fad4f5d6cc29f0163c34571f
Calvin Jackson <amigohmccclurkin98@gmail.com>
2023-08-16 17:04:53 +0300

    changing all USD to TZS

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1400,7 +1400,7 @@ WIKI_ENABLED = True
 
 COURSE_MODE_DEFAULTS = {
     'bulk_sku': None,
-    'currency': 'usd',
+    'currency': 'tzs',
     'description': None,
     'expiration_datetime': None,
     'min_price': 0,
@@ -2016,7 +2016,7 @@ EMBARGO_SITE_REDIRECT_URL = None
 PAYMENT_SUPPORT_EMAIL = 'billing@example.com'
 
 # Setting for PAID_COURSE_REGISTRATION, DOES NOT AFFECT VERIFIED STUDENTS
-PAID_COURSE_REGISTRATION_CURRENCY = ['usd', '$']
+PAID_COURSE_REGISTRATION_CURRENCY = ['tzs', 'tzs']
 
 ################################# EdxNotes config  #########################
 
